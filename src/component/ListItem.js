4b8c8e964872a4596daed183e6858553ffa77590
khanhthien <phankhanhthien1998@gmail.com>
2021-06-10 02:23:18 +0700

    deloy project"

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -63,3 +63,4 @@ console.log(filterItem)
 
 
 export default ListItem;
+
--- src/component/Menubar.js
+++ src/component/Menubar.js
@@ -6,23 +6,23 @@ const menubar = () => {
         <div className="container">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src={logo}alt="Girl in a jacket" width="50" height="50"/></a>
+                    <a className="navbar-brand" ><img src={logo}alt="Girl in a jacket" width="50" height="50"/></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <a className="nav-link active" aria-current="page" >Home</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
+                        <a className="nav-link" >Fea</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="#">Pricing</a>
+                        <a className="nav-link" >Pricing</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                        <a className="nav-link disabled"  tabIndex="-1" aria-disabled="true">Disabled</a>
                         </li>
                     </ul>
                     </div>
